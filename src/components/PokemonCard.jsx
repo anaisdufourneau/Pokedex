14b090dd3652077dd6332e4494fdb3b21394e2af
Dufourneau Anais <anais.dufourneau87@gmail.com>
2024-06-12 16:31:00 +0200

    props 2 finish

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,20 @@
+import PropTypes from "prop-types";
+
 function PokemonCard({ pokemon }) {
-  
-  
   return (
-  <> 
-    <figure><h1>{pokemon.name}</h1>{pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>} </figure> 
-  </>
-  )
-
+    <>
+      <figure>
+        <h1>{pokemon.name}</h1>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}{" "}
+      </figure>
+    </>
+  );
 }
-
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }),
+};
 
 export default PokemonCard;
