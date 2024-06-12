bc03968b0f3f67cfa0c46d84760c18cd3596ef01
Dufourneau Anais <anais.dufourneau87@gmail.com>
2024-06-12 15:58:42 +0200

    props1 finish

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,13 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
+  
+  
   return (
-    <>
-    
-      <figure>
-        <h1>{pokemon.name}</h1>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
-      </figure>
-    </>
-  );
- 
+  <> 
+    <figure><h1>{pokemon.name}</h1>{pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>} </figure> 
+  </>
+  )
+
 }
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-    imgSrc: "",
-  },
-];
+
 export default PokemonCard;
